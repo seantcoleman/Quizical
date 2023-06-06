374abdc4f7d2b9503465289496b13fec47331c4f
Sean Coleman <scoleman2296@gmail.com>
2023-06-06 02:17:39 -0400

    Conditional button and connecting to API

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,34 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect } from 'react'
 import Question from './components/Question'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [game, setGame] = useState(false)
+  const [questions, setQuestions] = useState({})
+
+  useEffect(() => {
+    fetch("https://opentdb.com/api.php?amount=5")
+    .then(res => res.json())
+    .then(data => setQuestions(data.results))
+  }, [])
+
+  console.log(questions)
+
+  
 
   return (
     <>
-      <Question />
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Quizicle</h1>
+        {!game && <button onClick={() => setGame((game) => true)}>
+          Start game
+        </button>}
+
+        <pre>{JSON.stringify(questions, null, 4)}</pre>
     </>
   )
 }
 
 export default App
+
+// 
